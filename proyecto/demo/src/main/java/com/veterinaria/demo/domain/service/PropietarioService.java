0de66231606d3dc46f65233a87fe0be1e53fd163
Antonio9000-ops <js886175@gmail.com>
2025-10-13 19:06:40 -0500

    limpio

--- proyecto/demo/src/main/java/com/veterinaria/demo/domain/service/PropietarioService.java
+++
@@ -1,9 +0,0 @@
-package com.veterinaria.demo.domain.service;
-
-import com.veterinaria.demo.domain.dto.PropietarioDTO;
-import java.util.Optional;
-
-public interface PropietarioService {
-    Optional<PropietarioDTO> getPropietarioByEmail(String email);
-    PropietarioDTO updatePropietario(String email, PropietarioDTO propietarioDetails);
-}
